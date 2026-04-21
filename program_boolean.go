869cf5aa9cf71557d535676aac45b1fb0c97ba37
rakabgs27 <rakabgs2000@gmail.com>
2026-04-21 22:30:36 +0700

    [ADD] : Implement basic boolean logic demonstration in Go

---
+++ program_boolean.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nilaiLulus = 80
+	var nilaiAkhir = 80
+
+	var lulus = nilaiLulus >= 75
+	var akhir = nilaiAkhir >= 90
+
+	fmt.Println("lulus =", lulus)
+	fmt.Println("akhir =", akhir)
+}
